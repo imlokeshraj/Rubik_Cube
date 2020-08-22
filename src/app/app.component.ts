8bb641b84dd4c5866bd4655660877eafee341f49
Lokesh Raj <lokeshraj1497@gmail.com>
2020-08-22 13:03:31 +0530

    Cube Face view with control buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Rubik-Solver-View';
+
+  left() {
+    console.log("Go Left")
+  }
+
+  right() {
+    console.log("Go Right")
+  }
+
+  top() {
+    console.log("Go Top")
+  }
+
+  bottom() {
+    console.log("Go Bottom")
+  }
 }
